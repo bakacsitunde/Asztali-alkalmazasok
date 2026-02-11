290e881a52fd2cd810d14d345e2080d2abf38dbc
bakacsitunde <bakacsi.tundi@gmail.com>
2026-02-11 11:56:54 +0100

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Lista
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> lista = new List<string>();
+
+            // Adding elements to the list
+            lista.Add("refrf");
+            lista.Add("rerf");
+            lista.Add("rtghth");
+            lista.Add("kizujthtz");
+            lista.Add("gthtzrf");
+
+            lista.Reverse();
+
+            int index = 1;
+
+            // Displaying elements in the list
+            Console.WriteLine("Elements in the list: ");
+            foreach (string elem in lista)
+            {
+                Console.WriteLine($"A(z) {index} elem a listában: " + elem); index++;
+            }
+            Console.WriteLine();
+            Console.WriteLine("Elements in the list (with for: ");
+            for (int i = 0; i < lista.Count; i++)
+            {
+                Console.WriteLine($"A(z) {i + 1} elem a listában: " + lista.ElementAt(i));
+            }
+            Console.Write("Element 2 is present in the list? ") + lista .Contains(2));
+
+            lista.Sort();
+            index = 0;
+            foreach (int elem in lista)
+            {
+                Console.WriteLine($"A(z) {index + 1} elem a listában: " + elem); index++;
+        }
+    }
+}
---
+++ LINQgyak1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+//var evenNumbers = from num in numbers
+//                  where num % 2 == 0
+//                  select num;
+
+
+
+List<int> resultNumbers = new List<int>();
+foreach (int num in numbers) //from num in numbers
+{
+    if (num % 2 == 0)// where helyett
+    {
+        resultNumbers.Add(num); //select  num helyett
+    }
+}
+
+    foreach (var num in resultNumbers)
+    {
+        Console.WriteLine(num);
+    }
+
+
+
